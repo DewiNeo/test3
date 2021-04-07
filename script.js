6b4dca79cdff9721256159b807ad7d31819ed341
meimei <dewi.ismaneti@binus.ac.id>
2021-04-07 13:27:16 +0700

    let

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-// console.log('Hello World')
+// // console.log('Hello World')
 
-//materi 1
-//ESS: VAR
+// //materi 1
+// //ESS: VAR
 
-var myName = "Meimei Neo";
-// myName = "meme";
-// myName = "bingung";
+// var myName = "Meimei Neo";
+// // myName = "meme";
+// // myName = "bingung";
 
-console.log(myName);
+// console.log(myName);
 
 
-//ES 6: const (Constant)
-const myCar = "Honda Jazz Rx";
-// myCar = "BMW";
+// //ES 6: const (Constant)
+// const myCar = "Honda Jazz Rx";
+// // myCar = "BMW";
 
-console.log(myCar);
+// console.log(myCar);
+
+
+//materi 2
+var myCar1 =  ["BMW", "Tesla", "Toyota"];
+
+for (i = 0; i < myCar1.length; i++ ){
+    console.log("myCar1: " + myCar1[i]);
+}
+
+console.log(i);
+
+const myCar2 = ["BMW", "Tesla", "Toyota"];
+for (x = 0; x < myCar2.length; x++ ){
+    console.log("myCar2: " + myCar1[x]);
+}
