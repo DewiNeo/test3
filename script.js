3e35a29b8c6ac406a46d44219c3e33d5e9262ba9
meimei <dewi.ismaneti@binus.ac.id>
2021-04-07 13:03:03 +0700

    var vs const

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Hello World')
+// console.log('Hello World')
+
+//materi 1
+//ESS: VAR
+
+var myName = "Meimei Neo";
+// myName = "meme";
+// myName = "bingung";
+
+console.log(myName);
+
+
+//ES 6: const (Constant)
+const myCar = "Honda Jazz Rx";
+// myCar = "BMW";
+
+console.log(myCar);
